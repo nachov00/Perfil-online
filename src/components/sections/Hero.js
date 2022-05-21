f9a8f5845486f7e90537ed081dc8f76133af8df6
nachov00 <nachov00@hotmail.com>
2022-05-20 21:12:20 -0300

    accesos a LinkedIn y Github

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,19 +61,19 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Bienvenidos al CV de <span className="text-color-primary"><br></br>Ignacio Villanueva</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Aspirante a desarrollador, autodidacta, resuelto y práctico para solucionar problemas y mejorar procesos.
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                    Get started
+                  <Button tag="a" color="primary" wideMobile target="blank" href="https://www.linkedin.com/in/ignacio-villanueva-51442323b/" >
+                    LinkedIn
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
+                  <Button tag="a" color="dark" wideMobile target="blank" href="https://github.com/nachov00">
+                    Github
                     </Button>
                 </ButtonGroup>
               </div>
